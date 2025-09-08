e1e1fb0a088018e9eac2168f061cfa3d95469278
blueeyedcynic <42555491+blueeyedcynic@users.noreply.github.com>
2025-09-08 11:27:02 -0500

    ignoring unescaped entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,4 +22,11 @@ const eslintConfig = [
   },
 ];
 
-export default eslintConfig;
+export default [
+  // your existing config...
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off'
+    }
+  }
+];
